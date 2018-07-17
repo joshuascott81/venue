9e10e7b1bec56900e13c01986c566f8900d46b47
joshuascott81 <joshuascott81@gmail.com>
2018-07-17 11:18:54 -0500

    Completed profile routes for now.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -15,71 +15,120 @@ const User = require('../../models/User');
 // @route GET api/profile
 // @desc Get current user's profile
 // @access Private
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .populate('user', ['name', 'email'])
+      .then(profile => {
+        if (!profile) {
+          errors.noProfile = 'There is no profile for this user';
+          res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route GET api/profile/handle/:handle
+// @desc Get profile by handle
+// @access Public
+router.get('/handle/:handle', (req, res) => {
   const errors = {};
-  
-  Profile.findOne({ user: req.user.id })
-  .populate('user', ['name', 'email'])
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'email'])
     .then(profile => {
-      if(!profile) {
-        errors.noProfile = 'There is no profile for this user';
+      if (!profile) {
+        errors.noProfile = 'There is no profile for this user handle';
         res.status(404).json(errors);
+      } else {
+        res.json(profile);
       }
-      res.json(profile);
     })
     .catch(err => res.status(404).json(err));
+});
+
+// @route GET api/profile/user/:id
+// @desc Get profile by user ID
+// @access Public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'email'])
+    .then(profile => {
+      if (!profile) {
+        errors.noProfile = 'There is no profile for this user id';
+        res.status(404).json(errors);
+      } else {
+        res.json(profile);
+      }
+    })
+    .catch(err =>
+      res.status(404).json({
+        'user/:user_id-Error': 'There is no profile for this user',
+        mongooseError: err
+      })
+    );
 });
 
 // @route GET api/profile
 // @desc Create or edit current user's profile
 // @access Private
-router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const { errors, isValid } = validateProfileInput(req.body);
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
 
-  // Check validation
-  if(!isValid) {
-    // Return any errors
-    return res.status(400).json(errors);
-  }
-  
-  // Get fields
-  const profileFields = {};
-  profileFields.user = req.user.id;
-  if(req.body.handle) profileFields.handle = req.body.handle;
-  if(req.body.brandName) profileFields.brandName = req.body.brandName;
-  console.log(req.body.websites);
-  if(typeof req.body.websites !== 'undefined') profileFields.websites = req.body.websites.split(',');
-  if(req.body.bio) profileFields.bio = req.body.bio;
-  if(req.body.youtubeUrl) profileFields.youtubeUrl = req.body.youtubeUrl;
-  if(req.body.vimeoUrl) profileFields.vimeoUrl = req.body.vimeoUrl;
-  if(req.body.facebookUrl) profileFields.facebookUrl = req.body.facebookUrl;
+    // Check validation
+    if (!isValid) {
+      // Return any errors
+      return res.status(400).json(errors);
+    }
 
-  Profile.findOne({ user: req.user.id })
-    .then(profile => {
-      if(profile) {
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.brandName) profileFields.brandName = req.body.brandName;
+    console.log(req.body.websites);
+    if (typeof req.body.websites !== 'undefined')
+      profileFields.websites = req.body.websites.split(',');
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.youtubeUrl) profileFields.youtubeUrl = req.body.youtubeUrl;
+    if (req.body.vimeoUrl) profileFields.vimeoUrl = req.body.vimeoUrl;
+    if (req.body.facebookUrl) profileFields.facebookUrl = req.body.facebookUrl;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
         // Update
         Profile.findOneAndUpdate(
-          { user: req.user.id }, 
-          { $set: profileFields }, 
-          {new: true }
-        )
-        .then(profile => res.json(profile));
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
       } else {
         // Create
-        
-        // Check if handle exists
-        Profile.findOne({ handle: profileFields.handle })
-          .then(profile => {
-            if (profile) {
-              errors.handle = 'That handle already exists';
-              res.status(400).json(errors);
-            }
 
-            // Save Profile
-            new Profile(profileFields).save().then(profile => res.json(profile));
-          });
+        // Check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = 'That handle already exists';
+            res.status(400).json(errors);
+          }
+
+          // Save Profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
       }
-    })
-});
+    });
+  }
+);
 
 module.exports = router;
